a7adf4a8af3581603c404978a3787881da25708b
feuzeu <thierry.feuzeu@gmail.com>
2017-10-11 07:11:38 -0400

    Fixed code style.

--- src/Controllers/JaxonController.php
+++ src/Controllers/JaxonController.php
@@ -57,13 +57,13 @@ class JaxonController extends Controller
         // Process Jaxon request
         $jaxon = Yii::$app->getModule('jaxon');
 
-        $jaxon->onInit(function($instance) {
+        $jaxon->onInit(function ($instance) {
             $this->initInstance($instance);
         });
-        $jaxon->onBefore(function($instance, $method, &$bEndRequest) {
+        $jaxon->onBefore(function ($instance, $method, &$bEndRequest) {
             $this->beforeRequest($instance, $method, $bEndRequest);
         });
-        $jaxon->onAfter(function($instance, $method) {
+        $jaxon->onAfter(function ($instance, $method) {
             $this->afterRequest($instance, $method);
         });
 
